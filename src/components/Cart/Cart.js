0b057109f53c4e8f5b1f9c87c138c2c9be3e56dd
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-01 15:32:57 +0600

    conditional rendering practice added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Cart = ({ cart, handleCartButton }) => {
+    // there are 4 various system to for conditional rendering
+    let command;
+    if (cart.length <= 0) {
+        command = <p>add something to cart</p>
+    }
+    else {
+        command = <p>you are adding</p>
+    }
     return (
         <div>
             <h3>Selected Items :{cart.length}</h3>
@@ -10,6 +18,13 @@ const Cart = ({ cart, handleCartButton }) => {
                     <button onClick={() => handleCartButton(item)}>X</button>
                 </p>)
             }
+            {command}
+            {cart.length === 3 ? <p>you have already added 3 items</p> : <p>keep adding</p>}
+            {cart.length === 2 && <div>
+                <h5>Congratulations</h5>
+                <p>You have added 2 items</p>
+            </div>}
+            {cart.length <= 3 || <button>Review your order</button>}
         </div>
     );
 };
