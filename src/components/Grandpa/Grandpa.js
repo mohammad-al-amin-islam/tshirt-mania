2925d4a0cde6c1c24e02a4b73deb0cd1a2c023ef
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-01 19:17:31 +0600

    context api with multiple parameter practice added

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -4,6 +4,14 @@ import Father from '../Father/Father'
 import Uncle from '../Uncle/Uncle'
 import './Grandpa.css'
 
+/* 
+1.call createContext with default value
+2.set a variable of the context with upprecase
+3.make sure export this variable
+4.wrap the child content using <RingContext.provider>
+5.provide a value
+to consume the context from the child component we have to use useContext hook and pass the Context name as parameter
+ */
 export const RingContext = createContext('ring');
 
 const Grandpa = () => {
@@ -13,7 +21,7 @@ const Grandpa = () => {
         setHouse(house + 1);
     }
     return (
-        <RingContext.Provider value={ornaments}>
+        <RingContext.Provider value={[house, setHouse]}>
             <div className='grandpa' >
                 <h3>Grandpa</h3>
                 <h5>House:{house}</h5>
